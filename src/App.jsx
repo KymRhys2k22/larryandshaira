b2a2514c01eaf2bfe67adf56be75af87dab93e55
KymRhys2k22 <kymrhys@gmail.com>
2024-01-21 01:18:18 +0800

    Adding animation/responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -5,43 +5,60 @@ import flowerb from './assets/page1/flowerb.svg'
 import larryshaira from './assets/page1/larryshaira.svg'
 import sanpedro from './assets/page1/sanpedro.svg'
 import date from './assets/page1/date.svg'
+import { Suspense } from 'react'
+import loading from './assets/page1/loading.json'
+import Lottie from 'lottie-react'
+
+const Loading = () => {
+  return <Lottie animationData={loading} loop={true} className='w-56' />
+}
 
 const App = () => {
   return (
     <>
-      <header className='p-0 relative w-[100vw] h-[100vh] '>
-        <m.img
-          initial={{ opacity: 0, x: -100 }}
-          whileInView={{ opacity: 1, x: 1 }}
-          viewport={{ once: true }}
-          transition={{ delay: 1 }}
+      <header className='snap-y snap-mandatory w-screen h-screen overflow-scroll '>
+        <img className='max-sm:landscape:[140px] md:w-[400px] max-md:w-[400px] max-sm:w-[270px] fixed top-[-120px] left-[-120px] w-[100%] min-w-[200px] max-w-[500px]' src={flower} alt="" />
+        <img className='max-sm:landscape:w-[350px] fixed right-[-120px] bottom-[-120px]
+        w-[100%]  max-sm:w-[330px] max-w-[500px]' src={flowerb} alt="" />
+        <Suspense fallback={Loading}>
+          <section className='snap-start w-screen h-screen '>
+            <m.div
+              initial={{ opacity: 0, y: 1 }}
+              whileInView={{ opacity: 1, y: -100 }}
+              viewport={{ once: false }}
+              transition={{ delay: .7 }}
 
-          className=' max-md:w-[400px] max-sm:w-[280px] absolute top-[-120px] left-[-120px] w-[100%] min-w-[200px] max-w-[500px]' src={flower} alt="" />
-        <m.img
-          initial={{ opacity: 0, x: 100 }}
-          whileInView={{ opacity: 1, x: 1 }}
-          viewport={{ once: true }}
-          transition={{ delay: .7 }}
-          className=' absolute right-[-120px] bottom-[-120px]
-        w-[100%]  max-sm:w-[400px] max-w-[500px]' src={flowerb} alt="" />
-        <m.img
-          initial={{ opacity: 0, y: 100 }}
-          whileInView={{ opacity: 1, y: 1 }}
-          viewport={{ once: true }}
-          transition={{ delay: 1.8 }}
-          className='absolute  right-0 left-0 mx-auto mt-24 lg:w-[600px] ' src={larryshaira} alt="" />
-        <m.img
-          initial={{ opacity: 0, y: 100 }}
-          whileInView={{ opacity: 1, y: 1 }}
-          viewport={{ once: true }}
-          transition={{ delay: .9 }}
-          className='absolute right-0 left-0 bottom-80 mx-auto' src={date} alt="" />
-        <m.img
-          initial={{ opacity: 0, y: 100 }}
-          whileInView={{ opacity: 1, y: 1 }}
-          viewport={{ once: true }}
-          transition={{ delay: 1.5 }}
-          className='absolute right-0 left-0 mx-auto bottom-52' src={sanpedro} alt="" />
+              className='mx-auto max-sm:landscape:w-[20vw]  pt-40 sm:w-[60%] md:h-screen flex flex-col lg:w-[30%]'>
+              <img className='w-auto min-w-28' src={larryshaira} alt="" />
+              <img className='' src={date} alt="" />
+              <img className='' src={sanpedro} alt="" />
+            </m.div>
+          </section >
+          <section className='snap-start w-screen h-screen '>
+            <m.div
+              initial={{ opacity: 0, y: 1 }}
+              whileInView={{ opacity: 1, y: -100 }}
+              viewport={{ once: false }}
+              transition={{ delay: .7 }}
+              className='mx-auto max-sm:landscape:w-[20vw]  pt-40 sm:w-[60%] md:h-screen flex flex-col lg:w-[30%]'>
+              <img className='w-auto min-w-28' src={larryshaira} alt="" />
+              <img className='' src={date} alt="" />
+              <img className='' src={sanpedro} alt="" />
+            </m.div>
+          </section >
+          <section className='snap-start w-screen h-screen '>
+            <m.div
+              initial={{ opacity: 0, y: 1 }}
+              whileInView={{ opacity: 1, y: -100 }}
+              viewport={{ once: false }}
+              transition={{ delay: .7 }}
+              className='mx-auto max-sm:landscape:w-[20vw]  pt-40 sm:w-[60%] md:h-screen flex flex-col lg:w-[30%]'>
+              <img className='w-auto min-w-28' src={larryshaira} alt="" />
+              <img className='' src={date} alt="" />
+              <img className='' src={sanpedro} alt="" />
+            </m.div>
+          </section >
+        </Suspense>
 
 
 
